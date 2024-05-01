f475bf7d6b4a960bc74f773397a3354e925336ab
GenTuBong <168326269+GenTuBong@users.noreply.github.com>
2024-05-01 11:36:21 +0800

    Create example.js

---
+++ example.js
@@ -0,0 +1,14 @@
+var box = document.getElementById("id-1");
+box.addEventListener("click", function() {
+    window.location.href = "https://gentubong.github.io/yige.github.io/";
+});
+
+var box = document.getElementById("id-2");
+box.addEventListener("click", function() {
+    window.location.href = "https://baidu.com";
+});
+
+function guanyu() {
+    window.location.href = "./guanyu/guanyu.html";
+};
+
