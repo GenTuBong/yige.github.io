d6a8dd392f765fb8888e6c253a3d51b54a314f6b
GenTuBong <168326269+GenTuBong@users.noreply.github.com>
2024-05-03 15:35:55 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
+function guanyu() {
+    window.location.href = "./guanyu/guanyu.html";
+};
+
 var box = document.getElementById("id-1");
 box.addEventListener("click", function() {
     window.location.href = "https://gentubong.github.io/yige.github.io/";
@@ -8,7 +12,27 @@ box.addEventListener("click", function() {
     window.location.href = "https://baidu.com";
 });
 
-function guanyu() {
-    window.location.href = "./guanyu/guanyu.html";
-};
+var box = document.getElementById("id-3");
+box.addEventListener("click", function() {
+    window.location.href = "https://www.bilibili.com";
+});
 
+var box = document.getElementById("id-4");
+box.addEventListener("click", function() {
+    window.location.href = "https://chatglm.cn/";
+});
+
+var box = document.getElementById("id-5");
+box.addEventListener("click", function() {
+    window.location.href = "https://www.jd.com/";
+});
+
+var box = document.getElementById("id-6");
+box.addEventListener("click", function() {
+    window.location.href = "https://open.weixin.qq.com/";
+});
+
+var box = document.getElementById("id-7");
+box.addEventListener("click", function() {
+    window.location.href = "https://www.jxcf.org.cn/";
+});
